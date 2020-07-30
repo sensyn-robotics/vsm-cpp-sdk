53a674652d88515a65f73e1487fe0f0eecabf7d6
MCharikov <mcharikov@ugcs.com>
2020-07-30 11:30:31 +0300

    Configurations for docs build action

---
+++ doc/conf.py
@@ -0,0 +1,93 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'CPP VSM SDK'
+copyright = '2020, SPH Engineering'
+author = 'SPH Engineering'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+#extensions += ['sphinx_rtd_theme']
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'pydata_sphinx_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = ['_static']
+html_logo = 'images/logo.png'
+
+html_theme_options = {
+  "github_url": "https://github.com/ugcs/vsm-cpp-sdk",
+  "external_links": [
+    {"name": "UgCS", "url": "https://www.ugcs.com/"}
+   ]
+}
+
+
+# Specify the path to Doxyrest extensions for Sphinx:
+sys.path.insert(1, os.path.abspath('../../doxyrest-bin/share/doxyrest/sphinx/'))
+extensions += ['doxyrest', 'cpplexer']
+exclude_patterns += ['page_index.rst']
+
+#extensions = [ 'breathe','exhale' ]
+#breathe_projects = { "cppsdk": "f:/Github/MikeCharikov/vsm-cpp-sdk/doc/xml/" }
+#breathe_default_project = "cppsdk"
+
+# Setup the exhale extension
+#exhale_args = {
+    # These arguments are required
+#    "containmentFolder":     "./rst/api",
+#    "rootFileName":          "library_root.rst",
+#    "rootFileTitle":         "Library API",
+#    "doxygenStripFromPath":  "..",
+    # Suggested optional arguments
+#    "createTreeView":        True,
+    # TIP: if using the sphinx-bootstrap-theme, you need
+    # "treeViewIsBootstrap": True,
+#    "exhaleExecutesDoxygen": True,
+#    "exhaleDoxygenStdin":    "INPUT = ../include"
+#}
+
+# Tell sphinx what the primary language being documented is.
+#primary_domain = 'cpp'
+
+# Tell sphinx what the pygments highlight language should be.
+#highlight_language = 'cpp'
